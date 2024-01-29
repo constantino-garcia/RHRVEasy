43ea93f2ec3d31a26be5948c162a17952ed28586
constantino-garcia <constantinoantoniogm@gmail.com>
2024-01-29 10:51:33 +0100

    Add RRData folder

--- inst/scripts/paperExperiments4Groups.R
+++ inst/scripts/paperExperiments4Groups.R
@@ -35,16 +35,89 @@ library("tidyverse")
 
  stopifnot(all(bonferroni$HRVIndices == fdr$HRVIndices, na.rm = TRUE))
  stopifnot(all(none$HRVIndices == fdr$HRVIndices, na.rm = TRUE))
+ HRVIndices <- bonferroni$HRVIndices
+ bonferroni <- bonferroni$stats
+ fdr <- fdr$stats
+ none <- none$stats
 
- bonferroni <- dplyr::rename(bonferroni$stats, bonferroni = adj.p.value)
- fdr <- dplyr::rename(fdr$stats, fdr = adj.p.value)
- none <- dplyr::rename(none$stats, none = adj.p.value)
+ stopifnot(all(none$p.value == none$adj.p.value))
+ stopifnot(all(none$p.value == bonferroni$p.value))
+ stopifnot(all(none$p.value == fdr$p.value))
 
- pvalues <- merge(dplyr::select(bonferroni, -pairwise), dplyr::select(fdr, -pairwise))
- stopifnot(all(none$p.value == none$none))
- none <- dplyr::arrange(none, HRVIndex)
- pvalues <- dplyr::arrange(pvalues, HRVIndex)
- stopifnot(all(none$p.value == pvalues$p.value))
+getSignificantPosthocs <- function(df, significance = 0.05) {
+       posthoc <- do.call(rbind, df[df$adj.p.value < significance, ]$pairwise)
+       posthoc <- posthoc[posthoc$adj.p.value < significance, ]
+       posthoc$adj.method <- deparse(substitute(df))
+       posthoc
+}
 
- significance <- 0.05
- pvalues %>% filter(p.value < significance, bonferroni < significance, fdr < significance) %>% pull("HRVIndex")
+filterByComparison <- function(posthoc, g1, g2) {
+        posthoc %>% filter(((group1 == g1) & (group2 == g2)) | ((group1 == g2) & (group2 == g1)))
+}
+
+sb <- getSignificantPosthocs(bonferroni)
+sf <- getSignificantPosthocs(fdr)
+sn <- getSignificantPosthocs(none)
+
+
+fdr_sign <- sf %>% filterByComparison("normal", "chf") %>% pull("HRVIndex")
+none_sign <- sn %>% filterByComparison("normal", "chf") %>% pull("HRVIndex")
+always <- intersect(
+        intersect(
+                sb %>% filterByComparison("normal", "chf") %>% pull("HRVIndex"),
+                fdr_sign
+        ),
+        none_sign
+)
+
+diffIndex <- setdiff(fdr_sign, always)
+diffIndex2 <- setdiff(none_sign, always)
+
+sf %>% dplyr::filter(HRVIndex %in% diffIndex) %>%
+        filter(group1 == "normal", group2 == "chf")
+bonferroni %>% filter(HRVIndex %in% diffIndex)
+filter(
+        (bonferroni %>% filter(HRVIndex == "SDNNIDX") %>% pull("pairwise"))[[1]],
+        group1 == "normal", group2 == "chf"
+)
+
+sn %>% dplyr::filter(HRVIndex %in% diffIndex2) %>%
+        filter(group1 == "normal", group2 == "chf")
+
+
+# -------------------------------------------------------------------------
+getSignificantGroups <- function(significantDataframe) {
+        significantDataframe %>%
+                mutate(comparison = paste(sep = " Vs ", group1, group2)) %>%
+                group_by(comparison, adj.method) %>%
+                summarise(numberSignificantIndices = n())
+}
+significantGroups <- getSignificantGroups(rbind(sn, sf, sb)) %>%
+        pivot_wider(names_from = adj.method, values_from = numberSignificantIndices)
+significantGroups <-
+        significantGroups %>%
+        relocate(none, .before = bonferroni) %>%
+        relocate(fdr, .before = bonferroni)
+print("Number of significant indices")
+print(significantGroups)
+knitr::kable(significantGroups, "latex")
+
+significantGroups %>% rowwise() %>% mutate(m = median(fdr, bonferroni))
+significantGroups %>% ungroup() %>%  summarise(across(where(is.numeric), mean))
+
+significant_indices <- function(df) {
+        d <- df %>%
+                mutate(comparison = paste(sep = " Vs ", group1, group2)) %>%
+                group_by(comparison, adj.method) %>%
+                select(comparison, HRVIndex) %>% nest(data=HRVIndex)
+        referenceIndices <- filter(d, comparison == "normal Vs chf") %>% pull("data")
+        referenceIndices <- referenceIndices[[1]]$HRVIndex
+        for (i in 1:4) {
+                        if(length(setdiff(d$data[[i]]$HRVIndex, referenceIndices)) != 0) {
+                               stop(d$comparison[[i]], "\n")
+                        }
+                }
+        }
+significant_indices(sn)
+significant_indices(sb)
+significant_indices(sf)
